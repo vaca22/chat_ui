b55ed819cd039a7138d376cac552aea904c4071f
vaca22 <wsmslgh@gmail.com>
2022-03-20 23:28:24 +0800

    uighik

--- lib/MyHomePage2.dart
+++ lib/MyHomePage2.dart
@@ -1,17 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage2 extends StatefulWidget {
-  const MyHomePage2({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage2({Key? key, required this.title, required this.orderNo}) : super(key: key);
+  final String orderNo;
 
   final String title;
 
@@ -19,69 +13,435 @@ class MyHomePage2 extends StatefulWidget {
   State<MyHomePage2> createState() => _MyHomePageState2();
 }
 
-class _MyHomePageState2 extends State<MyHomePage2> {
-  int _counter = 0;
 
-  void _incrementCounter() {
+
+
+
+
+
+class _MyHomePageState2 extends State<MyHomePage2> {
+  late TextEditingController textEditingController;
+  ScrollController _scrollController = ScrollController();
+  late double contentMaxWidth;
+  late String userId;
+  late String employeeNo;
+  late String userName;
+
+  @override
+  void initState() {
+    super.initState();
+    textEditingController = TextEditingController();
+    initData();
+  }
+
+  initData() async {
+    employeeNo = "50";
+    userId = "54";
+    userName = "56";
+    String url ='eReplyList';
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+
+      list = (res.data as List).reversed.toList();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    contentMaxWidth = MediaQuery.of(context).size.width - 90;
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('协催回复'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Color(0xFFF1F5FB),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Expanded(
+              flex: 1,
+              child: Container(
+                //列表内容少的时候靠上
+                alignment: Alignment.topCenter,
+                child: _renderList(),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0x14000000),
+                    blurRadius: 10,
+                  ),
+                ],
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Expanded(
+                    child:Container(
+                      margin: EdgeInsets.fromLTRB(15, 10, 0, 10),
+                      constraints: BoxConstraints(
+                        maxHeight: 100.0,
+                        minHeight: 50.0,
+                      ),
+                      decoration: BoxDecoration(
+                          color:  Color(0xFFF5F6FF),
+                          borderRadius: BorderRadius.all(Radius.circular(2))
+                      ),
+                      child: TextField(
+                        controller: textEditingController,
+                        cursorColor:Color(0xFF464EB5),
+                        maxLines: null,
+                        maxLength: 200,
+                        decoration: InputDecoration(
+                          counterText: '',
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.only(
+                              left: 16.0, right: 16.0, top: 10.0, bottom:10.0),
+                          hintText: "回复",
+                          hintStyle: TextStyle(
+                              color: Color(0xFFADB3BA),
+                              fontSize:15
+                          ),
+                        ),
+                        style: TextStyle(
+                            color: Color(0xFF03073C),
+                            fontSize:15
+                        ),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    behavior: HitTestBehavior.opaque,
+                    child: Container(
+                      padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                      alignment: Alignment.center,
+                      height: 70,
+                      child: Text(
+                        '发送',
+                        style: TextStyle(
+                          color: Color(0xFF464EB5),
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                    onTap: () {
+                      sendTxt();
+                    },
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+
+
+
+
+  List list = []; //列表要展示的数据
+
+  _renderList() {
+    return GestureDetector(
+      child: ListView.builder(
+        reverse: true,
+        shrinkWrap: true,
+        padding: EdgeInsets.only(top: 27),
+        itemBuilder: (context, index) {
+          var item = list[index];
+          return GestureDetector(
+            child: item['employeeNo'] == employeeNo
+                ? _renderRowSendByMe(context, item)
+                : _renderRowSendByOthers(context, item),
+            onTap: () {},
+          );
+        },
+        itemCount: list.length,
+        physics: const AlwaysScrollableScrollPhysics(),
+        controller: _scrollController,
+      ),
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+    );
+  }
+
+
+
+  Widget _renderRowSendByOthers(BuildContext context, item) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            child: Text(
+              CommonUtils.timeStampFormat(item['createdAt']),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Color(0xFFA1A6BB),
+                fontSize: 14,
+              ),
+            ),
+            padding: EdgeInsets.only(bottom: 20),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 15,right: 45),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.center,
+                  width: 30,
+                  height: 30,
+                  decoration: BoxDecoration(
+                      color: Color(0xFF464EB5),
+                      borderRadius: BorderRadius.all(Radius.circular(15))),
+                  child: Padding(
+                    child: Text(
+                      item['name'].toString().substring(0, 1),
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                      ),
+                    ),
+                    padding: EdgeInsets.only(bottom: 2),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        child: Text(
+                          item['name'],
+                          softWrap: true,
+                          style: TextStyle(
+                            color: Color(0xFF677092),
+                            fontSize: 14,
+                          ),
+                        ),
+                        padding: EdgeInsets.only(left: 20, right: 30),
+                      ),
+                      Stack(
+                        children: <Widget>[
+                          Container(
+                            child: Image(
+                                width: 11,
+                                height: 20,
+                                image: AssetImage(
+                                    "static/images/chat_white_arrow.png")),
+                            margin: EdgeInsets.fromLTRB(2, 16, 0, 0),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                                boxShadow: [
+                                  BoxShadow(
+                                    offset: Offset(4.0, 7.0),
+                                    color: Color(0x04000000),
+                                    blurRadius: 10,
+                                  ),
+                                ],
+                                color: Colors.white,
+                                borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
+                            margin: EdgeInsets.only(top: 8, left: 10),
+                            padding: EdgeInsets.all(10),
+                            child: Text(
+                              item['reply'],
+                              style: TextStyle(
+                                color: Color(0xFF03073C),
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _renderRowSendByMe(BuildContext context, item) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            child: Text(
+              CommonUtils.timeStampFormat(item['createdAt']),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Color(0xFFA1A6BB),
+                fontSize: 14,
+              ),
+            ),
+            padding: EdgeInsets.only(bottom: 20),
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            textDirection: TextDirection.rtl,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(right: 15),
+                alignment: Alignment.center,
+                width: 30,
+                height: 30,
+                decoration: BoxDecoration(
+                    color: Color(0xFF464EB5),
+                    borderRadius: BorderRadius.all(Radius.circular(15))),
+                child: Padding(
+                  child: Text(
+                    item['name'].toString().substring(0, 1),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
+                  ),
+                  padding: EdgeInsets.only(bottom: 2),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Padding(
+                    child: Text(
+                      item['name'],
+                      softWrap: true,
+                      style: TextStyle(
+                        color: Color(0xFF677092),
+                        fontSize: 14,
+                      ),
+                    ),
+                    padding: EdgeInsets.only(right: 20),
+                  ),
+                  Stack(
+                    alignment: Alignment.topRight,
+                    children: <Widget>[
+                      Container(
+                        child: Image(
+                            width: 11,
+                            height: 20,
+                            image: AssetImage(
+                                "static/images/chat_purple_arrow.png")),
+                        margin: EdgeInsets.fromLTRB(0, 16, 2, 0),
+                      ),
+                      Row(
+                        textDirection: TextDirection.rtl,
+                        children: <Widget>[
+                          ConstrainedBox(
+                            child: Container(
+                              margin: EdgeInsets.only(top: 8, right: 10),
+                              decoration: BoxDecoration(
+                                  boxShadow: [
+                                    BoxShadow(
+                                      offset: Offset(4.0, 7.0),
+                                      color: Color(0x04000000),
+                                      blurRadius: 10,
+                                    ),
+                                  ],
+                                  color: Color(0xFF838CFF),
+                                  borderRadius:
+                                  BorderRadius.all(Radius.circular(10))),
+                              padding: EdgeInsets.all(10),
+                              child: Text(
+                                item['reply'],
+                                softWrap: true,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ),
+                            constraints: BoxConstraints(
+                              maxWidth: contentMaxWidth,
+                            ),
+                          ),
+                          Container(
+                              margin: EdgeInsets.fromLTRB(0, 8, 8, 0),
+                              child: item['status'] == SENDING_TYPE
+                                  ? ConstrainedBox(
+                                constraints:
+                                BoxConstraints(maxWidth: 10, maxHeight: 10),
+                                child: Container(
+                                  width: 10,
+                                  height: 10,
+                                  child: CircularProgressIndicator(
+                                    strokeWidth: 2.0,
+                                    valueColor: new AlwaysStoppedAnimation<Color>(
+                                        Colors.grey),
+                                  ),
+                                ),
+                              )
+                                  : item['status'] == FAILED_TYPE
+                                  ? Image(
+                                  width: 11,
+                                  height: 20,
+                                  image: AssetImage(
+                                      "static/images/network_error_icon.png"))
+                                  : Container()),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  final int maxValue = 1<<32;
+
+  sendTxt() async {
+    int tag = random.nextInt(maxValue);
+
+    String message = textEditingController.value.text;
+    addMessage(message, tag);
+    textEditingController.text = '';
+
+  }
+
+  final random = Random();
+
+  addMessage(content, tag) {
+    int time = new DateTime.now().millisecondsSinceEpoch;
+    setState(() {
+      list.insert(0, {
+        "createdAt": time,
+        "cusUid": userId,
+        "employeeNo": employeeNo,
+        "name": userName,
+        "orderNo": widget.orderNo,
+        "reply": content,
+        "updatedAt": time,
+        'status': SENDING_TYPE,
+        'tag': '${tag}',
+      });
+    });
+    Timer(
+        Duration(milliseconds: 100),
+            () => _scrollController.jumpTo(0));
+  }
+
+  static int SENDING_TYPE = 0;
+  static int FAILED_TYPE = 1;
+  static int SUCCESSED_TYPE = 2;
+
 }
